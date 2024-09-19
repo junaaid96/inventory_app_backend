02348c80a007cd88f449b59dbcac788ee2e300f3
junaaid96 <info.junaidulislam@gmail.com>
2024-09-19 20:58:59 +0600

    cors middleware added

--- inventory_app_backend/settings.py
+++ inventory_app_backend/settings.py
@@ -96,6 +96,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'inventory_app_backend.urls'
