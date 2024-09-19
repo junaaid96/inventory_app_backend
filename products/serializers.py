41202013b503d66099d079891276cac1d50eb6cd
junaaid96 <info.junaidulislam@gmail.com>
2024-09-19 19:52:11 +0600

    v0.2.1

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Product
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
 
 class ProductSerializer(serializers.ModelSerializer):
+    added_by = UserSerializer(read_only=True)
+
     class Meta:
         model = Product
         fields = '__all__'
