d6ad3432085c40c9627a55fdfb4cb7b9d7279456
junaaid96 <info.junaidulislam@gmail.com>
2024-09-19 15:27:14 +0600

    v0.2

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# inventory_app_backend
+# Product Inventory App Backend
+
+This is the backend for an inventory management application built with Django REST Framework.
+
+## Getting Started
+
+### Prerequisites
+- Python 3.x
+- Django
+
+### Installation
+1. Clone the repository
+2. Install dependencies: `pip install -r requirements.txt`
+
+### Running the Server
+```
+python manage.py runserver
+```
+
+## API Endpoints
+
+### User Management
+- **Register**: `POST /users/register/`
+- **Login**: `POST /users/login/`
+- **Logout**: `POST /users/logout/`
+- **Profile**: `GET, PUT, PATCH /users/profile/`
+
+### Product Management
+- **List/Create**: `GET, POST /products/products/`
+- **Retrieve/Update/Delete**: `GET, PUT, PATCH, DELETE /products/products/{id}/`
+
+## Admin Access
+- **URL**: `/admin/`
+- **Default Superuser**:
+  - Username: `admin`
+  - Password: `12345`
--- products/views.py
+++ products/views.py
@@ -1,13 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.core.exceptions import PermissionDenied
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
         serializer.save(added_by=self.request.user)
+
+    def get_object(self):
+        obj = super().get_object()
+        if self.action in ['update', 'partial_update', 'destroy']:
+            if obj.added_by != self.request.user:
+                raise PermissionDenied("You do not have permission to modify this product.")
+        return obj
